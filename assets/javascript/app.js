0e8735a3cab064d9e2e6d86ccf6d9889d9bf52c3
Greg Zahora <gregzahora3@gmail.com>
2019-07-06 18:15:29 -0400

    all of the basic game functions are made, but I need to re-write some that produce ugly displays (such as the buttons the user selects to guess) I also need to display what the correct answer was if the user gets it wrong.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
 //questions and answers as objects within an array
-
 var trivia = [{
     question: "Ireland accounts for 0.007% of the global population yet produces ___ of all CO2 emission released into the atmosphere (global emissions).",
     choices: ["0.7%", "0.001%", "0.03%", "0.15%"],
@@ -52,48 +51,102 @@ var trivia = [{
 }];
 
 // Main game variable that holds scores, timing, and changes the question
-
 var game = {
     correct: 0,
     incorrect: 0,
     skipped: 0,
-    timer: 20,
-    timerOn: false,
-    timerCount:"",
+    timer: 5,
     questions: trivia,
     currentQuestion: 0,
+};
 
-}
+// function to count down
+function countDown(){
+    game.timer--;
+    $("#timer").text("Seconds Left: " + game.timer);
+    if(game.timer === 0){
+        outOfTime();
+    }
+};
 
-//function to display and loop trivia questions
-
-function firstQuestion(){
-    setInterval(game.timer, 1000);
+//function to display first question
+function newQuestion(){
+    timerSet = setInterval(countDown, 1000);
     $("#question").text(trivia[game.currentQuestion].question);
     for (var i = 0; i < trivia[game.currentQuestion].choices.length; i++){
         $("#choices").append("<button class='choices-btns' id='button-"+i+"' data-name='"+trivia[game.currentQuestion].choices[i]+"' >" + trivia[game.currentQuestion].choices[i] + "</button>");
     }
 };
 
+//function to display next question
+function nextQuestion() {
+    game.timer = 5;
+    $("#timer").text("Seconds Left: " + game.timer);
+    game.currentQuestion++;
+    $("#choices").text("");
+    newQuestion();
+}
 
+//event listener to see if the user's guess (from clicking the button) matches the answer stored in the button
+$(document).on("click", ".choices-btns", function(e){
+    userGuess(e);
+});
 
 //function to check answer after click
-
 function userGuess (e){
-    clearInterval(timer);
     if($(e.target).data("name")===trivia[game.currentQuestion].answer){
-        console.log("correct");
+        correctGuess();
     } else {
-        console.log("wrong");
+        incorrectGuess();
     }
 };
 
-$(document).on("click", ".choices-btns", function(e){
-    userGuess(e); //see what this means
-})
+//function for correct answer
+function correctGuess (){
+    clearInterval(timerSet);
+    console.log("correct");
+    game.correct++;
+    if (game.currentQuestion === trivia.length - 1){
+        setTimeout(finalScore, 1000);
+    } else {
+        setTimeout(nextQuestion, 1000);
+    }
+};
+
+//function for incorrect answer
+function incorrectGuess (){
+    clearInterval(timerSet);
+    console.log("incorrect");
+    game.incorrect++;
+    if (game.currentQuestion === trivia.length - 1){
+        setTimeout(finalScore, 1000); //still need results function
+    } else {
+        setTimeout(nextQuestion, 1000);
+    }
+};
+
+//function for letting the timer run out
+function outOfTime () {
+    clearInterval(timerSet);
+    console.log("Out of time");
+    game.skipped++;
+    if (game.currentQuestion === trivia.length - 1){
+        setTimeout(finalScore, 1000); //still need results function
+    } else {
+        setTimeout(nextQuestion, 1000);
+    }
+};
+
+//function to show final score
+function finalScore (){
+    clearInterval(timerSet);
+    $("#triviaGame").html("See your results!");
+    $("#triviaGame").append("Correct Answers: " + game.correct);
+    $("#triviaGame").append("Incorrect Answers: " + game.incorrect);
+    $("#triviaGame").append("Skipped Answers: " + game.skipped);
+};
 
 //function to remove start button after it is pressed and reset game
-
 $("#start").on("click", function(){
     $("#start").remove();
 
@@ -104,9 +157,8 @@ $("#start").on("click", function(){
     clearInterval(game.timerCount);
 
     $("#triviaGame").show();
-    $("#timer").text(game.timer);
-    $("countDown").show();
+    $("#timer").text("Seconds Left: " + game.timer);
 
-    firstQuestion();
+    newQuestion();
 
 })
